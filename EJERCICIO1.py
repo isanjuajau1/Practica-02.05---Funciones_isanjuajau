918918743888c2bb6b97a68e881831e96705eb12
isanjuajau1 <isanjuajau1@educacion.navarra.es>
2024-11-11 21:52:46 +0100

    EJERCICO 1

---
+++ EJERCICIO1.py
@@ -0,0 +1,10 @@
+def saludo(nombre):
+    """Función que muestra un saludo con el nombre proporcionado.
+    
+    Parámetros:
+        - nombre: Cadena de texto con el nombre de la persona.
+    
+    Salida:
+        Saludo personalizado en pantalla.
+    """
+    print(f"¡Hola, {nombre}!")
